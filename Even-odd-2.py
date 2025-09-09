2380ca389396d414c4a3e2d18c982defebf95112
sonaldixit1997 <sonaldixit1997@gmail.com>
2025-09-09 23:20:21 +0530

    Updated program files

---
+++ Even-odd-2.py
@@ -0,0 +1,8 @@
+# Step 1: Take integer input from the user
+num = int(input("Enter an integer: "))
+
+# Step 2: Check even or odd using if-else
+if num % 2 == 0:
+    print(f"{num} is Even")
+else:
+    print(f"{num} is Odd")
---
+++ Greetings-1.py
@@ -0,0 +1,3 @@
+first_name=input("enter your first name:")
+second_name=input("enter your second name:")
+print(f'''Hello,{first_name} {second_name}! Welcome to the python programme''')
